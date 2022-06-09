072950d11240026bac52fa9787de1e1397ed7110
Joe <joe.attah1@icloud.com>
2022-06-08 23:07:33 -0500

    First Commit

---
+++ src/components/search-box/search-box.component.jsx
@@ -0,0 +1,32 @@
+// import React, { Component } from "react";
+import "./search-box.styles.css";
+
+const SearchBox = (props) => {
+  return (
+    <div>
+      <input
+        className={`search-box ${props.className}`}
+        type="search"
+        placeholder={props.placeholder}
+        onChange={props.onChangeHandler}
+      />
+    </div>
+  );
+};
+
+// class SearchBox extends Component {
+//   render() {
+//     return (
+//       <div>
+//         <input
+//           className={`search-box ${this.props.className}`}
+//           type="search"
+//           placeholder={this.props.placeholder}
+//           onChange={this.props.onChangeHandler}
+//         />
+//       </div>
+//     );
+//   }
+// }
+
+export default SearchBox;
